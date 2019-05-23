93f06470926b503940488b5359daec80e5797aa3
christianreiser <reisercg@gmail.com>
2019-05-23 09:32:15 +0200

    1a finished

--- 6/ex6.py
+++ 6/ex6.py
@@ -2,6 +2,12 @@ import tensorflow as tf
 import numpy as np
 
 
+"""
+Ex 1 a)
+- run
+- tensorboard --logdir ./
+"""
+
 def main():
     data = np.loadtxt(open("data2Class_adjusted.txt", "r"), dtype=np.float32)
     num_samples = len(data[:, 0])
@@ -10,7 +16,7 @@ def main():
     p = tf.placeholder(tf.float32, shape=num_samples, name='p')  # TODO: remove after model returns p
     y = tf.placeholder(tf.float32, shape=num_samples, name='labels')
     beta = tf.placeholder(tf.float32, shape=num_samples, name='beta')
-    # p = tf.sigmoid(
+    # p = tf.sigmoid(  # TODO: compute p when features beta are available
     #    tf.math.multiply(
     #        tf.transpose(X), beta)
     #    )
@@ -19,32 +25,20 @@ def main():
     print(total_loss.shape)
 
     with tf.Session() as sess:
-        writer = tf.summary.FileWriter(sess.graph)
-        y_data = (data[:, 1])
-        y_rand = np.random.rand(200)
-        # print(sess.run(total_loss, feed_dict={y: np.ones((num_samples, 1), p: np.random.rand(100, 1)}))
-        # print(y_data, y_rand)
+        writer = tf.summary.FileWriter('./graphs', sess.graph)
         print(
             sess.run(
                 total_loss,
                 feed_dict={
                     y: data[:, 3],
                     p: np.random.rand(200)}))  # TODO: feed X instead of p when model exists
+    writer.close()
 
 
 def loss(labels, logits, num_samples):
     """
     Computes the total loss for logistic regression
-    """"""
-    ones_vec = np.ones(num_samples, dtype=np.float32)
-    # print(ones_vec, 'ones_vec')
-    y_log_p = tf.multiply(labels, tf.math.log(logits))
-    one_minus_p = tf.math.subtract(ones_vec, logits)
-    log_one_minus_p = tf.math.log(one_minus_p)
-    one_minus_y = tf.math.subtract(ones_vec, labels)
-    second_summand = tf.multiply(one_minus_y,log_one_minus_p)
-    loss_vec = tf.add(y_log_p, second_summand)
-"""
+    """
     ones_vec = np.ones(num_samples, dtype=np.float32)
     loss_vec = tf.math.add(
         tf.multiply(labels, tf.math.log(logits)),
