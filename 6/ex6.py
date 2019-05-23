ea484b9f9ed66c53a64268d72a5ddb2e9e34c085
christianreiser <reisercg@gmail.com>
2019-05-23 09:47:50 +0200

    add comments

--- 6/ex6.py
+++ 6/ex6.py
@@ -8,29 +8,29 @@ Ex 1 a)
 - tensorboard --logdir ./
 """
 
-def main():
-    data = np.loadtxt(open("data2Class_adjusted.txt", "r"), dtype=np.float32)
-    num_samples = len(data[:, 0])
 
-    X = tf.placeholder(tf.float32, shape=(num_samples, 2), name='X')
-    p = tf.placeholder(tf.float32, shape=num_samples, name='p')  # TODO: remove after model returns p
-    y = tf.placeholder(tf.float32, shape=num_samples, name='labels')
-    beta = tf.placeholder(tf.float32, shape=num_samples, name='beta')
+def main():
+    data = np.loadtxt(open("data2Class_adjusted.txt", "r"), dtype=np.float32)  # read data from previous exercise
+    num_samples = len(data[:, 0])  # number of samples in dataset
+
+    X = tf.placeholder(tf.float32, shape=(num_samples, 2), name='X')  # input from dataset
+    p = tf.placeholder(tf.float32, shape=num_samples, name='p')  # model output (logits)
+    y = tf.placeholder(tf.float32, shape=num_samples, name='labels')  # labels
+    beta = tf.placeholder(tf.float32, shape=num_samples, name='beta')  # features
     # p = tf.sigmoid(  # TODO: compute p when features beta are available
     #    tf.math.multiply(
     #        tf.transpose(X), beta)
     #    )
 
-    total_loss = loss(y, p, num_samples)
-    print(total_loss.shape)
+    total_loss = loss(y, p, num_samples)  # returns total loss form labels and logits
 
     with tf.Session() as sess:
-        writer = tf.summary.FileWriter('./graphs', sess.graph)
+        writer = tf.summary.FileWriter('./graphs', sess.graph)  # write graph event file for TensorBoard visualization
         print(
             sess.run(
                 total_loss,
                 feed_dict={
-                    y: data[:, 3],
+                    y: data[:, 3],  # feed label data into placeholder
                     p: np.random.rand(200)}))  # TODO: feed X instead of p when model exists
     writer.close()
 
@@ -38,9 +38,10 @@ def main():
 def loss(labels, logits, num_samples):
     """
     Computes the total loss for logistic regression
+    L(beta) = -sum[ y*log(p) + (1-y)*log(1-p) ]
     """
-    ones_vec = np.ones(num_samples, dtype=np.float32)
-    loss_vec = tf.math.add(
+    ones_vec = np.ones(num_samples, dtype=np.float32) # vector with ones
+    loss_vec = tf.math.add(  # loss vector, each entry corresponds to one data sample
         tf.multiply(labels, tf.math.log(logits)),
         tf.multiply(
             tf.math.subtract(ones_vec, labels),
@@ -49,7 +50,7 @@ def loss(labels, logits, num_samples):
             )
         )
     )
-    total_loss = -tf.reduce_sum(loss_vec)
+    total_loss = -tf.reduce_sum(loss_vec)  # reduce vector by summing up
     return total_loss
 
 
