2fe7f4671d5525b454329cbc193fbd54a51c4c53
christianreiser <reisercg@gmail.com>
2019-06-15 15:55:54 +0200

    8 unfinished

--- 8/8.py
+++ 8/8.py
@@ -2,18 +2,19 @@ from os import listdir
 import numpy as np
 import skimage
 import scipy.sparse.linalg as sla
+from PIL import Image
 
 # 2a
 # load all images in a directory
 tmp_images = []
-for filename in listdir('yalefaces_cropBackground'):
+for filename in listdir('yalefaces'):
     tmp_images.append(np.ndarray.flatten(
-                    skimage.data.imread('yalefaces_cropBackground/' + filename)))
+                    skimage.data.imread('yalefaces/' + filename)))
     images = np.concatenate([image[np.newaxis] for image in tmp_images])
 
 # 2b
 # compute the mean face 
-num_images = len(images)
+num_images = images.shape[0]
 sum_faces = np.sum(images, axis=0)
 mean_face = 1/num_images * sum_faces
 
@@ -25,16 +26,41 @@ images_centered = np.subtract(images, np.transpose(mean_face))
 # 2c
 # Compute the singular value decomposition X_tilde = U*D*V_transposed for the centered data matrix
 # X_tilde = U*D*np.transpose(V)
-num_eigenvalues = 3  # TODO: correct? must be between 1 and min(images_centered.shape), k=3 due to u, s, vt?
+num_eigenvalues = 60  # p value of ex. 2d
 # u Unitary matrix having left singular vectors as columns.
-# s are the singular values
-# vt are Unitary matrix having right singular vectors as rows
-u, s, vt = sla.svds(images_centered, k=num_eigenvalues)  # TODO: u, s, vt don't contain singular values?
-print('u=', u, ', s=', s, ', vt=', vt)
+# s are the singular values in ascending order (will be flipped)
+# vt Unitary matrix having right singular vectors as rows
+u, s, vt = sla.svds(images_centered, k=num_eigenvalues)
+s = np.flip(s)  # flip to descending order
+u = np.flip(u)
+vt = np.flip(vt)
+# print('\n\n u=', u, ',\n\n s=', s, ',\n\n vt=', vt)
 
 # 2d
 # Find the p-dimensional representations Z = X_tilde * V_p, where Vp ∈ R^77760×p 
 # contains only the first p columns of V. 
 # Assume p = 60. The rows of Z represent each face as a p-dimensional vector, 
 # instead of a 77760-dimensional image.
-p = 60
+v_p = np.transpose(vt)  # p contains only the first p columns of V
+Z = np.matmul(images_centered, v_p)
+print('v_p shape', v_p.shape)
+
+# Reconstructed_face = mean_face + Z * v_p
+reconstructed_images = np.add(
+    np.transpose(mean_face),
+    np.matmul(Z, np.transpose(v_p)))
+
+# save and display images
+reconstructed_images_tensor = np.reshape(reconstructed_images, [320, 243, num_images])
+#print(reconstructed_images_tensor[0].shape)
+#img = Image.fromarray(reconstructed_images, 'L')
+#img.save('reconstruction.png')
+#img.show()
+
+# compute reconstruction error
+reconstruction_error = np.sum(np.subtract(images, reconstructed_images)**2)  # TODO norm elementwise?
+print('reconstruction_error=', reconstruction_error)
+"""reconstruction_error = 0
+for image in range(images):
+    reconstruction_error = reconstruction_error + abs()
+"""
